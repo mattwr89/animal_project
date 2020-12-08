eb0b98013fb7ecc425bb763c0d600a97a0f0c966
maryniakm <mateusz.maryniak@directinfo.pl>
2020-12-08 01:22:10 +0100

    Role entity added

---
+++ src/main/java/pl/mattwr89/fresh/app/domain/entities/Role.java
@@ -0,0 +1,25 @@
+package pl.mattwr89.fresh.app.domain.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@ToString
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    private User user;
+}
